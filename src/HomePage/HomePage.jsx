97988a68b8144a6103f65baf7ef07e018c9d9f95
stefano <sirfoga@protonmail.com>
2020-09-19 09:52:00 +0200

    add text input

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import Link from 'react-router-dom/es/Link';
+import { ListItem, ListItemIcon, ListItemText, List } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
 import {
-  CircularProgressbar,
+    CircularProgressbar,
 } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import ChangingProgressProvider from "@/_components/ChangingProgressProvider";
 
 class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <div>
-          Go to
-          {' '}
-          <Link to="/google">Google</Link>
-        </div>
-        <ChangingProgressProvider values={[0, 20, 40, 60, 80, 100]}>
-          {percentage => (
-            <CircularProgressbar value={percentage} text={`${percentage}%`} />
-          )}
-        </ChangingProgressProvider>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <TextField id="standard-basic" label="Scan or search" />
+                <div>
+                    <List component="nav" aria-label="contacts">
+                        <ListItem button>
+                            <ListItemText primary="Chelsea Otakan"/>
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="Eric Hoffman"/>
+                        </ListItem>
+                    </List>
+                </div>
+                <ChangingProgressProvider values={[0, 20, 40, 60, 80, 100]}>
+                    {percentage => (
+                        <CircularProgressbar value={percentage} text={`${percentage}%`}/>
+                    )}
+                </ChangingProgressProvider>
+            </div>
+        );
+    }
 }
 
-export { HomePage };
+export {HomePage};
