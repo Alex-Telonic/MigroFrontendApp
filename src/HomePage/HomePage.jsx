79dd0d612209eb701c6777fefa5fbd1928915b0e
stefano <sirfoga@protonmail.com>
2020-09-19 10:26:31 +0200

    add fab

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -16,6 +16,13 @@ import ChangingProgressProvider from "@/_components/ChangingProgressProvider";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import NavigationIcon from '@material-ui/icons/Navigation';
+
+
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
@@ -60,6 +67,21 @@ class HomePage extends React.Component {
                 <div>
                     <button onClick={notify}>Notify !</button>
                     <ToastContainer />
+                </div>
+                <div>
+                    <Fab color="primary" aria-label="add">
+                        <AddIcon />
+                    </Fab>
+                    <Fab color="secondary" aria-label="edit">
+                        <EditIcon />
+                    </Fab>
+                    <Fab variant="extended">
+                        <NavigationIcon />
+                        Navigate
+                    </Fab>
+                    <Fab disabled aria-label="like">
+                        <FavoriteIcon />
+                    </Fab>
                 </div>
             </div>
         );
